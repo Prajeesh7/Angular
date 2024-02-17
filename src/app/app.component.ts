598a3fbc7a0c0b5b49ffa4a696a1206387570ccb
Prajeesh <prajeeshrajan07@gmail.com>
2024-02-17 12:34:59 +0530

    Two way binding completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -25,6 +26,7 @@ export class AppComponent {
   }
   conditions(){
     let num = 50
+
     if ( num<50 ){
       alert("number is lessthan 50")
     }else{
