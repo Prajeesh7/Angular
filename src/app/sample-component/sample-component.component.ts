6d73f5f2aaf92b356e3dbccf6d3b65e35f5e5283
Prajeesh <prajeeshrajan07@gmail.com>
2024-02-17 22:00:47 +0530

    ngIf applied

--- src/app/sample-component/sample-component.component.ts
+++ src/app/sample-component/sample-component.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sample-component',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './sample-component.component.html',
   styleUrl: './sample-component.component.css'
 })
 export class SampleComponentComponent {
+
+  num = 20
+  click = false
 
 }
